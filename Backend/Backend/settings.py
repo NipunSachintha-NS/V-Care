cc37e34f69d62ece44e550e28c2b5c8bc6c569f0
pinilDissanayaka <pinildissanayaka@gmail.com>
2025-01-01 15:22:13 +0530

    change settings

--- Backend/Backend/settings.py
+++ Backend/Backend/settings.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv, find_dotenv
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
